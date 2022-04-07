ff885140c9119b47232a242ad3a0225cc879305f
Julia Rudneva <Rudneva.julia@gmail.com>
2022-04-07 23:14:12 +0700

    fix compound

---
+++ hnh1/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  hnh1
+//
+//  Created by Юлия Руднева on 05.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
