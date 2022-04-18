70b93fa70e8255456055bf209111bb0c64f1586e
Julia Rudneva <Rudneva.julia@gmail.com>
2022-04-18 22:26:36 +0700

    add scrollView for registration view controller

--- hnh1/RegistrationViewController.swift
+++ hnh1/RegistrationViewController.swift
@@ -7,15 +7,18 @@
 
 import UIKit
 
-class RegistrationViewController: UIViewController, UITextFieldDelegate {
+class RegistrationViewController: UIViewController, UITextFieldDelegate, UIScrollViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let tap = UITapGestureRecognizer (target: self, action: #selector (endEditing))
         view.addGestureRecognizer(tap)
         loginTextField.delegate = self
         passwordTextField.delegate = self
         passwordConfirmTextField.delegate = self
+        registrationScrollView.delegate = self
+        
 //#Mark: placeholder style
         let attributes = [ NSAttributedString.Key.foregroundColor: UIColor.black,
             NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 21.0)]
@@ -31,7 +34,9 @@ class RegistrationViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var passwordConfirmTextField: UITextField!
     @IBOutlet weak var doneButton: flickeringButton!
+    @IBOutlet weak var registrationScrollView: UIScrollView!
     
+//#Mark: return key for keyboard
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField == loginTextField {
             passwordTextField.becomeFirstResponder()
@@ -44,8 +49,42 @@ class RegistrationViewController: UIViewController, UITextFieldDelegate {
         }
         return true
     }
-
+    
+//#Mark: for hiding keyboard with tap
     @objc func endEditing() {
         view.endEditing(true)
+    }
+    
+//#Mark: tracking a keyboard for scroll
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear (animated)
+        NotificationCenter.default.addObserver(self,
+                                                selector: #selector (keybrdDidChange),
+                                                name: UIResponder.keyboardWillShowNotification,
+                                                object: nil)
+        NotificationCenter.default.addObserver(self,
+                                                selector: #selector (keybrdDiDHide),
+                                                name: UIResponder.keyboardWillHideNotification,
+                                                object: nil)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear (animated)
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+//#Mark: changing the placement of a scrollview for scroll
+    @objc func keybrdDiDHide() {
+        registrationScrollView.contentInset = .zero
+    }
+
+    @objc func keybrdDidChange(sender: Notification) {
+        guard let keybrdFrame: NSValue = sender.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
+        else {
+        return
+        }
+            
+        let keybrdRectangle = keybrdFrame.cgRectValue
+        let keybrdHeight = keybrdRectangle.height
+        registrationScrollView.contentInset = .init(top: 0, left: 0, bottom: keybrdHeight, right: 0)
     }
 }
